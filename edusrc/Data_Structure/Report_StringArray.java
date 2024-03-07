20be0c624744c4213f2b7af17561605f507dd725
akasha-rec <uspira@naver.com>
2024-03-07 15:29:34 +0900

    3/13 과제 1 완성

--- edusrc/Data_Structure/Report_StringArray.java
+++ edusrc/Data_Structure/Report_StringArray.java
@@ -1,7 +1,6 @@
 package Data_Structure;
 
 public class Report_StringArray {
-public class 실습2_14스트링배열정렬 {
 	public static void main(String[] args) {
 		String []data = {"apple","grape","persimmon", "pear","blueberry", "strawberry", "melon", "oriental melon"};
 
@@ -9,9 +8,11 @@ public class 실습2_14스트링배열정렬 {
 		sortData(data);
 		showData(data);
 	}
+
 	static void showData(String[]arr) {
 		for(String name : arr)
-			System.out.println(name);
+			System.out.print(name+" ");
+		System.out.println();
 	}
 
 	static void swap(String[]arr, int ind1, int ind2) {
@@ -19,15 +20,15 @@ public class 실습2_14스트링배열정렬 {
 		arr[ind1] = arr[ind2];
 		arr[ind2] = t;
 	}
-	
+
 	static void sortData(String []arr) {
 		for (int i = 0; i < arr.length; i++)
 			for (int j = i+1; j < arr.length; j++) {
-				if(arr[i].compareTo(arr[j]) > 0) {//compareTo() 사용하는 것
+				if(arr[i].compareTo(arr[j])>0) {//compareTo() 사용하는 것 오름차순
 					swap(arr, i, j);
 				}
 			}
 	}
 
 }
-}
+
