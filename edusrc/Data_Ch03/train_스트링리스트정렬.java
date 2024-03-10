5d41578dda00c8aedd86bd391c592c32deb1b138
akasha-rec <160625667+akasha-rec@users.noreply.github.com>
2024-03-10 17:47:15 +0900

    과제 선행학습 1 완성

--- edusrc/Data_Ch03/train_스트링리스트정렬.java
+++ edusrc/Data_Ch03/train_스트링리스트정렬.java
@@ -1,27 +1,31 @@
 package Data_Ch03;
 
-import java.util.ArrayList;
-import java.util.Arrays;
+import java.util.ArrayList; //new ArrayList<>() > 원소 추가/삭제 O
+//Arrays.asList() : Arrays의 static class인 ArrayList return.
+import java.util.Arrays; 
 import java.util.List;
 public class train_스트링리스트정렬 {
 
 	    public static String[] removeElement1(String[] arr, String item) {
-	    	//array > list (Array.asList) >> list.remove >> list > array(list.toArray())
-
+	    	//array(size 고정 => 동적 추가/삭제 X) > Arrays.asList로 List화 > Arrays.asList 또한 크기 고정 리스트 > new ArrayList<>() 처리 > remove 메서드 사용 가능
+	    	List<String> list = new ArrayList<>(Arrays.asList(arr));
+	    	list.remove(item);
+	    	return list.toArray(new String[0]);
 	    }
 	    
 	    static void getList(List<String> list) {
-			list.add("서울");	list.add("북경");
-			list.add("상해");	list.add("서울");
-			list.add("도쿄");	list.add("뉴욕");
+			list.add("서울");	 list.add("북경");
+			list.add("상해");	 list.add("서울");
+			list.add("도쿄");	 list.add("뉴욕");
 
 
-			list.add("런던");	list.add("로마");
-			list.add("방콕");	list.add("북경");
-			list.add("도쿄");	list.add("서울");
+			list.add("런던");	 list.add("로마");
+			list.add("방콕");	 list.add("북경");
+			list.add("도쿄");	 list.add("서울");
 
 			list.add(1, "LA");
 	    }
+	    
 	    static void showList(String topic, List<String> list) {
 	    	System.out.println(topic);
 	    	for (String st : list) {
@@ -32,7 +36,12 @@ public class train_스트링리스트정렬 {
 	    static void sortList(List<String> list) {
 	    	//방법 1 : list.sort(null);
 	    	//방법 2 : list > String array로 변환 / list.toArray() <-> list.asList()
-	    	
+	    	list.sort(null);
+	    	//Collections.sort(list); - 2
+//	    	String[] array = list.toArray(new String[0]); - 3
+//	    	Arrays.sort(array);
+//	    	list.clear();
+//	    	list.addAll(Arrays.asList(array));
 	    }
 	    
 	    static String[] removeDuplicateList(List<String> list) {
@@ -40,9 +49,17 @@ public class train_스트링리스트정렬 {
 		    cities = list.toArray(cities);
 		    // list > array cities[]로 변환
 		    // for 문으로 도시가 중복인 것을 확인 > compareTo 이용 > removeElement로 제거까지
-		    removeElement1(cities, city);
+		    for (int i = 0; i < cities.length; i++) {
+		    	for (int j = i+1; j < cities.length; j++) {
+		    		if(cities[i].compareTo(cities[j])==0) {
+		    			cities = removeElement1(cities, cities[j]);
+		    			j--;
+		    		}
+		    	}
+		    }
 		    return cities;
 	    }
+	    
 		public static void main(String[] args) {
 			ArrayList<String> list = new ArrayList<>();
 			getList(list);
@@ -55,6 +72,7 @@ public class train_스트링리스트정렬 {
 			sortList(list);
 		    System.out.println();
 		    showList("정렬후", list);
+		    
 // 배열에서 중복제거
 		    System.out.println();
 		    System.out.println("중복제거::");
@@ -64,4 +82,3 @@ public class train_스트링리스트정렬 {
 		    showList("중복제거후", lst);
 		}
 	}
-
